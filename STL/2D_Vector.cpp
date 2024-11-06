5ad6666b04cc6661585aff54d40dfe445d9af5af
Arava Choudhary <aravachoudhary11@gmail.com>
2024-11-06 22:36:04 +0530

    STL Codes

--- STL/2D_Vector.cpp
+++ STL/2D_Vector.cpp
@@ -1,9 +1,24 @@
 #include<iostream>
+#include<vector>
 
 using namespace std;
 
 int main()
 {
-    
+    // 2D Vector
+    vector<vector<int> > arr(5 ,vector<int>(4,0));
+    int totalRows = arr.size();
+    int totalColumns = arr[0].size();
+
+    // Jaggered 2D Vector
+    vector<vector<int> > brr(4); // No. of Rows = 4
+    brr[0] = vector<int> (4);
+    brr[1] = vector<int> (2);
+    brr[2] = vector<int> (3);
+    brr[3] = vector<int> (3);
+    int totalRowCount = brr.size();
+    // int totalColumnsCount = brr[i].size();
+
+
     return 0;
 }

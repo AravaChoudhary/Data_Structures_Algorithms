c4e73ad830a6ce7bcac1cfc1710129e101fdb19f
Arava Choudhary <aravachoudhary11@gmail.com>
2025-02-24 11:28:07 +0530

    LeetCode 191

---
+++ Basics/Bitwise_Operators/set_bits.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    
+    int n;
+    cout<<"Enter the Number : ";
+    cin>>n;
+
+    int set_bit_count = 0;
+
+    while(n != 0) {
+
+        int last_bit = (n & 1);
+        
+        if(last_bit == 1) 
+            set_bit_count = set_bit_count + 1;
+        
+        // Right Shift
+        n = n >> 1;
+    }
+
+    cout<<"Total Number of Set Bits : "<<set_bit_count<<endl;
+
+    return 0;
+}
+
+// LeetCode - 191

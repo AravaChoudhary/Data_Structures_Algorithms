6c22bdeb98dd14dcbbcefa743c9870bf46814b96
Arava Choudhary <aravachoudhary11@gmail.com>
2025-06-21 19:41:02 +0530

    Palindrome Pyramid

---
+++ Basics/Restart/palindrome_pyramid.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+
+    int n;
+    cout<<"Palindrome Pyramid";
+    cout<<"Enter the Value of n : ";
+    cin>>n;
+
+    for(int i = 0; i < n; i++)
+    {
+        int c = 1;
+
+        for(int j = 0; j < n + i; j++)
+        {
+            if(j < n - i - 1)
+                cout<<" ";
+
+            else if(j <= n - 1)
+            {    cout<<c;
+                c++;
+            }
+            
+            else if(j == n)
+            {
+                c = c - 2;
+                cout<<c;
+                c--;
+            }
+
+            else 
+            {
+                cout<<c;
+                c--;
+            }
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}

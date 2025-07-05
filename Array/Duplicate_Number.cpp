ca4ee90640c9cc643f28157823ac3c2f0273562e
Arava Choudhary <aravachoudhary11@gmail.com>
2025-07-05 22:55:14 +0530

    LeetCode - 287

--- Array/Duplicate_Number.cpp
+++ Array/Duplicate_Number.cpp
@@ -10,7 +10,7 @@ int bruteforce(int arr[] , int n)
 
     for(int i = 0; i < n - 1; i++)
     {
-        if(arr[i] == arr[i+1])
+        if(arr[i] == arr[i + 1])
             return arr[i];
     }
     return -1;

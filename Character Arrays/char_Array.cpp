c6b7a1eb47d76fe05ab96cf25b61cccb1380c5a2
Arava Choudhary <aravachoudhary11@gmail.com>
2025-08-22 22:09:08 +0530

    Character Arrays

--- Character Arrays/char_Array.cpp
+++ Character Arrays/char_Array.cpp
@@ -30,6 +30,66 @@ void Replace_Character(char originalChar, char newChar, char arr[], int size) {
 }
 
 
+void convert_into_Upper_Case(char arr[], int n) {
+
+    int len = getlength(arr,n);
+    for(int i = 0; i < len ; i++) {
+        char ch = arr[i];
+
+        // If a letter is a lower case letter then only convert it.
+        if(ch >= 'a' and ch <= 'z')
+            ch = ch - 'a' + 'A';
+
+        arr[i] = ch;
+    }
+}
+
+
+void convert_into_Lower_Case(char arr[], int n) {
+
+    int len = getlength(arr,n);
+    for(int i = 0; i < len ; i++) {
+        char ch = arr[i];
+
+        // If a letter is a Upper case letter then only convert it.
+        if(ch >= 'A' and ch <= 'Z')
+            ch = ch - 'A' + 'a';
+            
+        arr[i] = ch;
+    }
+}
+
+void ReverseCharArray(char arr[], int n) {
+
+    int len = getlength(arr, n);
+    int i = 0, j = len - 1;
+
+    while(i <= j) {
+        swap(arr[i], arr[j]);
+        i++;
+        j--;
+    }
+}
+
+bool Check_Palindrome(char arr[], int n) {
+
+    int len = getlength(arr, n);
+    int i = 0, j = len - 1;
+
+    while(i <= j) {
+
+        if(arr[i] == arr[j]) {
+            i++;
+            j--;
+        }
+
+        else //Not a palinfrome
+            return false;
+    }
+
+    return true;
+}
+
 int main() {
 
     // // Creation
@@ -60,12 +120,28 @@ int main() {
     // cin>>array;
     // cout<<"Size of char array : "<<getlength(array,1000)<<endl;
 
+    // char arr1[1000];
+    // cin>>arr1;
+    // Replace_Character('@', ' ', arr1, 1000);
+    // cout<<arr1<<endl;
+
+
     char arr1[1000];
+    cout<<"Enter the Input : ";
     cin>>arr1;
-    Replace_Character('@', ' ', arr1, 1000);
+    cout<<"\nConverted to Upper Case : ";
+    convert_into_Upper_Case(arr1, 1000);
     cout<<arr1<<endl;
 
+    cout<<"\nConverted to Lower Case : ";
+    convert_into_Lower_Case(arr1,1000);
+    cout<<arr1<<endl;
 
+    cout<<"\nReversed Char Array : ";
+    ReverseCharArray(arr1,1000);
+    cout<<arr1<<endl;
+
+    cout<<"\nPalindrome Check\n Yes (1) and No(0) : "<<Check_Palindrome(arr1, 1000)<<endl;;
 
     return 0;
 

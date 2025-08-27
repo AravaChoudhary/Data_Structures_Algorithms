0e1c26efed228da2c273cd7194a7521b68540244
Arava Choudhary <aravachoudhary11@gmail.com>
2025-08-27 13:33:17 +0530

    LeetCode - 1047

--- String/Remove_Adjacent_Duplicates.cpp
+++ String/Remove_Adjacent_Duplicates.cpp
@@ -5,9 +5,33 @@
 
 using namespace std;
 
+string removeDuplicates(string s) {
+
+    int n = s.length();
+
+    string ans = ""; // Initialize ans string as empty string
+
+    for(int i = 0; i < n; i++) {
+
+        char current_char = s[i];
+
+        if(ans.empty() or current_char != ans.back())
+            ans.push_back(current_char);
+        
+        else
+            ans.pop_back();
+    }
+
+    return ans;
+}
+
 int main() {
 
+    string str;
+    cout<<"\nEnter the String : ";
+    cin>>str;
 
+    cout<<"\nThe Final String after Duplicate Removals : "<<removeDuplicates(str)<<endl;
     
     return 0;
 

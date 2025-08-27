289fb79089d35dfbc13fb83082254937d207d52d
Arava Choudhary <aravachoudhary11@gmail.com>
2025-08-27 13:51:05 +0530

    LeetCode - 1209

---
+++ String/Remove_Adjacent_Duplicates_II.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+
+string removeDuplicates_II(string s) {
+
+    int n = s.length();
+
+    string ans = ""; // Initialize ans string as empty string
+
+    for(int i = 0; i < n; i++) {
+
+        char current_char = s[i];
+
+        if(ans.empty() or current_char != ans.back())
+            ans.push_back(current_char);
+        
+        else
+            ans.pop_back();
+    }
+
+    return ans;
+}
+
+int main() {
+
+    string str;
+    cout<<"\nEnter the String : ";
+    cin>>str;
+
+    cout<<"\nThe Final String after Duplicate Removals : "<<removeDuplicates_II(str)<<endl;
+    
+    return 0;
+
+}
+
+// LeetCode - 1209.

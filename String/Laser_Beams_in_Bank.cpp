3081d5098a134a27481fb4fd80eff092b873d430
Arava Choudhary <aravachoudhary11@gmail.com>
2025-08-31 11:15:23 +0530

    LeetCode - 2125

---
+++ String/Laser_Beams_in_Bank.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+  
+
+int main() {
+
+    string order, s;
+    cout<<"\nEnter the Order : ";
+    getline(cin, order);
+
+    cout<<"\nEnter s : ";
+    getline(cin, s);
+    
+    return 0;
+
+}
+
+// Time Complexity ->
+// Space Complexity -> O(1)
+
+// LeetCode - 2125.

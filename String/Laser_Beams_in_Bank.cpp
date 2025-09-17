dbbb206055a71f0708aa973263c31910e9ccac68
Arava Choudhary <aravachoudhary11@gmail.com>
2025-09-17 06:31:44 +0530

    LeetCode - 2125

--- String/Laser_Beams_in_Bank.cpp
+++ String/Laser_Beams_in_Bank.cpp
@@ -3,21 +3,55 @@
 
 using namespace std;
   
+int countDevices(string& binary) {
 
-int main() {
-
-    string order, s;
-    cout<<"\nEnter the Order : ";
-    getline(cin, order);
-
-    cout<<"\nEnter s : ";
-    getline(cin, s);
+    int count = 0;
+    for(auto b : binary)
+        count += b - '0';
     
-    return 0;
-
+    return count;
 }
 
-// Time Complexity ->
-// Space Complexity -> O(1)
+int numberOfBeams(vector<string>& bank) {
+
+    vector<int> devices;
+    for(auto row : bank)
+        devices.push_back(countDevices(row));
+
+    int beams;
+    for(int i = 0; i < devices.size(); i++) {
+
+        int j = i + 1;
+        while(j < devices.size()) {
+
+            beams += devices[i] * devices[j];
+
+            if(devices[j] == 0)
+                i++;
+            else
+                break;
+        }
+    }
+
+    return beams;
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+    vector<string> bank(n);
+
+    cin.ignore();
+    for (int i = 0; i < n; ++i) {
+        getline(cin, bank[i]);
+    }
+    cout << numberOfBeams(bank) << endl;
+    
+    return 0;
+}
+
+// Time Complexity -> O(m * n)
+// Space Complexity -> O(m)
 
 // LeetCode - 2125.

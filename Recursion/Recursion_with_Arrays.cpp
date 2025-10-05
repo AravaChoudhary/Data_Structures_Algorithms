265118ac39f0ee2e2552b4514d5d4ee5e9d9c23a
Arava Choudhary <aravachoudhary11@gmail.com>
2025-10-05 15:39:12 +0530

    Searching through Recursion

--- Recursion/Recursion_with_Arrays.cpp
+++ Recursion/Recursion_with_Arrays.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-#include<vector>
 
 using namespace std;
 
@@ -14,14 +13,43 @@ void printArray(int arr[], int size, int index) {
     printArray(arr, size, index + 1);
 }
 
+
+bool SearchArray(int arr[], int size, int index, int target) {
+
+    // Base Case -> 2 Cases - Found or Not Found
+    if(index == size)   // Not Found
+        return false;
+
+    if(arr[index] == target)    // Found at the index
+        return true;
+
+    // Recursive Call
+    bool ans = SearchArray(arr, size, index + 1, target);
+    return ans;
+}
+
+
+
+
+
 int main() {
 
     int arr[] = {10, 20, 30, 40, 50, 60};
     int size = 6;
     int index = 0;    // Start printing from this index
 
-    cout<<"Array : ";
+    cout<<"\nArray : ";
     printArray(arr, size, index);
+
+    cout<<"\n\nSearching in Array"<<endl;
+    int target;
+    cout<<"\nEnter the Target : ";
+    cin>>target;
+    bool ans = SearchArray(arr,size, index, target);
+    if(ans == 0)
+        cout<<"\nTarget NOT FOUND\n";
+    else
+        cout<<"\nTarget FOUND\n";
 
     return 0;
 }

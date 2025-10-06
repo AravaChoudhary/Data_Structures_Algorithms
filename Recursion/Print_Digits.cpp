386e73570629f51134bcd488d50f94cd62aca215
Arava Choudhary <aravachoudhary11@gmail.com>
2025-10-06 07:22:51 +0530

    Print Digits

--- Recursion/Print_Digits.cpp
+++ Recursion/Print_Digits.cpp
@@ -2,7 +2,30 @@
 
 using namespace std;
 
+void PrintDigits_with_Recursion(int n) {
+
+    // Base Case
+    if(n == 0)
+        return ;
+
+    // Baaki Recursion Solve karega
+    int newNumber = n / 10;
+    PrintDigits_with_Recursion(newNumber);
+
+    // 1 Case mera
+    int digit = n % 10;
+    cout<<digit<<" ";
+}
+// Head Recursion
+
 int main() {
+
+    int n;
+    cout<<"Enter the Number : ";
+    cin>>n;
+
+    cout<<"\nDigits are ";
+    PrintDigits_with_Recursion(n);
 
     return 0;
 }

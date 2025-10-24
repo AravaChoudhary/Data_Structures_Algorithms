473f048af9c0ce2c442a393016c4c89498abc75b
Arava Choudhary <aravachoudhary11@gmail.com>
2025-10-24 09:07:17 +0530

    Min Max Algorithm

--- Divide_&_Conquer/Max_Min_Algorithm.cpp
+++ Divide_&_Conquer/Max_Min_Algorithm.cpp
@@ -25,7 +25,35 @@ pair<int, int> Max_Min_Algorithm(vector<int> arr) {
 
 
 // Recursive Approach
+pair<int, int> Recursive_Max_Min(vector<int> arr, int low, int high) {
 
+    int n = arr.size();
+    int maximum, minimum;
+
+    // Base Case I
+    if(low == high)
+        return {arr[low], arr[low]};
+
+    // Base Case II
+    if(low == (high - 1)) {
+
+        if(arr[low] < arr[high])
+            return {arr[high], arr[low]};
+
+        else 
+            return {arr[low], arr[high]};
+    }
+
+
+    int mid = (low + high) / 2;
+    pair<int, int> left = Recursive_Max_Min(arr, low, mid);
+    pair<int, int> right = Recursive_Max_Min(arr, mid + 1, high);
+
+    maximum = max(left.first, right.first);
+    minimum = min(left.first, right.first);
+    
+    return {maximum, minimum};
+}
 
 
 int main() {
@@ -42,8 +70,22 @@ int main() {
         arr.push_back(value);
     }
 
-    pair<int, int> result = Max_Min_Algorithm(arr);
+    cout<<"\nIterative Approach"<<endl;
+    pair<int, int> result_It = Max_Min_Algorithm(arr);
+    cout<<"\nMaximum is "<<result_It.first<<"\nMinimum is "<<result_It.second<<endl; 
+
+    cout<<"\nRecursiveApproach"<<endl;
+    pair<int, int> result = Recursive_Max_Min(arr, 0, (n - 1));
     cout<<"\nMaximum is "<<result.first<<"\nMinimum is "<<result.second<<endl; 
 
     return 0;
 }
+
+
+// Iterative
+    // - Time Complexity -> O(n)
+    // - Space Complexity -> O(1)
+
+// Recursive
+    // - Time Complexity -> O(n)
+    // - Space Complexity -> O(log n)

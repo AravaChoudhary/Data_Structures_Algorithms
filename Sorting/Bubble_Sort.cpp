69072e341796552c06a5af3ca3ea9bc509091280
Arava Choudhary <aravachoudhary11@gmail.com>
2025-11-04 07:04:01 +0530

    Optimized Bubble Sort

--- Sorting/Bubble_Sort.cpp
+++ Sorting/Bubble_Sort.cpp
@@ -15,6 +15,29 @@ void BubbleSort(vector<int>& arr, int n) {
     }
 }
 
+
+void OptimizedBubbleSort(vector<int>& arr, int n) {
+
+    int flag;
+
+    for(int i = 0; i < n; i++) {
+
+        flag = 0;
+        for(int j = 0; j < (n - i - 1); j++) {
+
+            if(arr[j] > arr[j + 1]) {
+
+                swap(arr[j], arr[j + 1]);
+                flag = 1;
+            }
+        }
+
+        if(flag == 0)
+            break;
+    }
+}
+
+
 int main() {
 
     vector<int> arr;
@@ -33,15 +56,19 @@ int main() {
     }
 
     cout<<"\n After Sorting"<<endl;
-    BubbleSort(arr, n);
+    OptimizedBubbleSort(arr, n);
 
     for(int i = 0; i < n; i++) {
         cout<<arr[i]<<endl;
     }
 
     return 0;
-
 }
 
 // Time Complexity --> O(N^2)
+
+// For Optimized Bubble Sort
+    // i) Best Case : O(N)
+    // ii) Worst Case : O(N^2)
+
 // Space Complexity --> O(1)
